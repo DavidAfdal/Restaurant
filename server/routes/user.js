da162c2b16645f70e3038597fd065ad0b32497da
gerin <grnbgsln@gmail.com>
2023-01-31 19:33:29 +0800

    update

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,7 +4,6 @@ const router = require("express").Router();
 //Create User
 router.post("/register", createUser);
 router.get("/actived/:id", activeUser)
-
 router.post("/login", userLogin);
 
 module.exports = router;
