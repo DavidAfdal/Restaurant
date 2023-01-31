a2f2fc7c35862229c6cb94c0b9fb5e99a995a01b
David Afdal Kaizar Mutahadi <davidafdal7@gmail.com>
2023-01-31 23:31:22 +0800

    update

--- client/src/Auth/Components/LoginItem.jsx
+++ client/src/Auth/Components/LoginItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Button, Card, CardContent, Container, Divider, InputAdornment, TextField, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, Container, Divider, InputAdornment, TextField, Typography, CircularProgress, Modal } from "@mui/material";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import LockPersonOutlinedIcon from "@mui/icons-material/LockPersonOutlined";
 import { useContext } from "react";
@@ -11,9 +11,34 @@ const LoginItem = () => {
   const auth = useContext(AuthContext);
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState("");
   const [password, setPassword] = useState("");
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #ff9f0d",
+    boxShadow: 24,
+    borderRadius: "5px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    p: 4,
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const handleClick = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     if (email === "admin" && password === "admin") {
       auth.adminLogin();
       navigate("/");
@@ -26,61 +51,82 @@ const LoginItem = () => {
       try {
         const user = await axios.post("http://localhost:3000/user/login", data);
         console.log(user);
-        auth.login(user.data.data._id);
-        navigate("/");
+        if (user.data.error) {
+          setOpen(true);
+          setError(user.data.error);
+        } else {
+          auth.login(user.data.data._id);
+          navigate("/");
+        }
       } catch (error) {
         console.log(error);
       }
+      setIsLoading(false);
     }
   };
-  return (
-    <Container maxWidth="lg" sx={{ py: "100px", height: "100%" }}>
+  return isLoading ? (
+    <Container maxWidth="lg" sx={{ py: "250px", height: "100vh" }}>
       <Box component="div" sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%", width: "100%" }}>
-        <Card sx={{ width: "350px" }}>
-          <CardContent>
-            <Typography variant="h4" sx={{ fontSize: "18px", fontWeight: "bold" }}>
-              Login
-            </Typography>
-            <form style={{ display: "flex", flexDirection: "column", gap: "20px", marginTop: "30px" }} onSubmit={handleClick}>
-              <TextField
-                placeholder="Email"
-                type="text"
-                onChange={(e) => setEmail(e.target.value)}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <EmailOutlinedIcon />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-
-              <TextField
-                placeholder="Password"
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <LockPersonOutlinedIcon />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-
-              <Button variant="contained" type="submit" sx={{ width: "100%", height: "100%", borderRadius: "0 4px 4px 0", bgcolor: "#ff9f0d", "&:hover": { bgcolor: "#ff9f0d", boxShadow: "none" }, boxShadow: "none" }}>
-                Submit
-              </Button>
-            </form>
-            <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 1 }}>
-              <Typography variant="p" sx={{ color: " #828282", fontSize: "14px" }}>
-                Forget password?
-              </Typography>
-            </Box>
-          </CardContent>
-        </Card>
+        <CircularProgress sx={{ color: "#ff9f0d" }} size={70} />
       </Box>
     </Container>
+  ) : (
+    <>
+      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+        <Box sx={style}>
+          <Typography variant="h6" component="h2" sx={{ color: "#232323" }}>
+            {error}
+          </Typography>
+        </Box>
+      </Modal>
+      <Container maxWidth="lg" sx={{ py: "100px", height: "100%" }}>
+        <Box component="div" sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%", width: "100%" }}>
+          <Card sx={{ width: "350px" }}>
+            <CardContent>
+              <Typography variant="h4" sx={{ fontSize: "18px", fontWeight: "bold" }}>
+                Login
+              </Typography>
+              <form style={{ display: "flex", flexDirection: "column", gap: "20px", marginTop: "30px" }} onSubmit={handleClick}>
+                <TextField
+                  placeholder="Email"
+                  type="text"
+                  onChange={(e) => setEmail(e.target.value)}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <EmailOutlinedIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+
+                <TextField
+                  placeholder="Password"
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <LockPersonOutlinedIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+
+                <Button variant="contained" type="submit" sx={{ width: "100%", height: "100%", borderRadius: "0 4px 4px 0", bgcolor: "#ff9f0d", "&:hover": { bgcolor: "#ff9f0d", boxShadow: "none" }, boxShadow: "none" }}>
+                  Submit
+                </Button>
+              </form>
+              <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 1 }}>
+                <Typography variant="p" sx={{ color: " #828282", fontSize: "14px" }}>
+                  Forget password?
+                </Typography>
+              </Box>
+            </CardContent>
+          </Card>
+        </Box>
+      </Container>
+    </>
   );
 };
 
--- client/src/Auth/Components/RegisterItem.jsx
+++ client/src/Auth/Components/RegisterItem.jsx
@@ -31,8 +31,12 @@ const RegisterItem = () => {
     transform: "translate(-50%, -50%)",
     width: 400,
     bgcolor: "background.paper",
-    border: "2px solid #000",
+    border: "2px solid #ff9f0d",
     boxShadow: 24,
+    borderRadius: "5px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     p: 4,
   };
 
